d9651dd0b8e12b405c9631a12f6c4465c7892029
MasterBlaster <rgersak@virsrvmgmt15.ip.t-com.hr>
2013-06-17 10:36:04 +0200

    Uploading to github

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                strives to simulate state machine behavior."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.taoensso/timbre "2.0.0"]
-                 [org.roribib/dreamcatcher "1.0.2-SNAPSHOT"]
+                 [org.roribib/dreamcatcher "1.0.2"]
                  [clj-time "0.5.0"]]
   :plugins [[lein-clojars "0.9.1"]]
   :source-paths ["src"])
