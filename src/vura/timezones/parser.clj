96ba74dc9d512571cfccd28399ded98ca3dc7c66
Robert Gersak <r.gersak@gmail.com>
2018-06-08 19:49:35 +0200

    Adding timezones. Parserer working on all of them. Now to do something with this data

--- src/vura/timezones/parser.clj
+++ src/vura/timezones/parser.clj
@@ -3,7 +3,10 @@
   (:require [vura.core :refer :all]
             [instaparse.core :as insta]))
 
-(def europe (slurp "resources/timezones/tzdb-2018e/europe"))
+(def zones ["europe" "africa" "northamerica" "pacificnew" "southamerica" "asia" "australasia"])
+
+(defn zone-definition [zone]
+  (slurp (clojure.java.io/resource (str "tzdb-2018e/" (name zone)))))
 
 (defn break-timezone-db 
   "Breaks timezone text to zones, rules and links"
@@ -62,12 +65,13 @@
   "number = #'[0-9]+'
    <comment> = #'#.*'
    <word> = #'[a-zA-Z]+'
-   <space> = #'[\\s\\t]+'
+   <space> = (' ' | '\\t')+
    <newline>= '\\n' | '\\n\\r'
    year = number
    month-name = 'Jan' | 'Feb' | 'Mar' | 'Apr' | 'May' | 'Jun' | 'Jul' | 'Aug' | 'Sep' | 'Oct' | 'Nov' | 'Dec'
    month = month-name
    day = number
+   floating-day = #'last\\w+' | #'\\w+>=\\d+|\\w+<=\\d+'
    second = number
    minute = number
    hour = number 
@@ -76,105 +80,109 @@
 
    <zone-start> =  'Zone'
    sign = '+' | '-'
-   gmtoff = sign? hour ':' minute (':' second)? 
-   zone-name = #'[a-zA-Z/_\\-]+'
-   zone-rule = #'[a-zA-Z\\-]+' 
-   zone-format = #'[A-Z\\-/%s]+'
-   zone-until = year (<space> month <space> day)? (<space> time)?
-   zone-row = <space> gmtoff <space> zone-rule <space> zone-format (<space> zone-until <newline>?)?
-   zone-definition = <zone-start> <space> zone-name zone-row+ 
+   gmtoff = sign? hour <':'> minute (<':'> second)? | number
+   zone-name = #'[a-zA-Z/_\\-0-9]+'
+   zone-rule = #'[a-zA-Z\\-_]+'  | gmtoff
+   zone-format = #'[A-Z\\-/%s\\+0-9a-z]+'
+   zone-until = year (<space> month)? (<space> day | <space> floating-day)? (<space> time)?
+   zone-row = <space> gmtoff <space> zone-rule <space> zone-format (<space> zone-until <space>? <newline> | <space> <newline>)?
+   zone-definition = <zone-start> <space> zone-name zone-row+ <space>? <newline>?
    
    link-alias = zone-name 
    link-canonical = zone-name
-   link-definition = <'Link'> <space> link-canonical <space> link-alias <newline>
+   link-definition = <'Link'> <space>+ link-canonical <space> link-alias <space>? <newline>?
 
    rule-name = #'[a-zA-Z\\-_]+'
    rule-type = '-' | word
    rule-from = year
-   rule-to = year | 'only'
+   rule-to = year | 'only' | 'max'
    rule-in = month
-   rule-on = day
+   rule-on = day | word | floating-day 
    rule-at = time
-   rule-save = time
-   rule-letters = word
-   rule-definition = <'Rule'> <space> rule-name <space> rule-from <space> rule-to <space> rule-type <space> rule-in <space> rule-on <space> rule-at <space> rule-save <space> rule-letters
+   rule-save = gmtoff | number
+   rule-letters = #'[a-zA-Z\\+\\-0-9]+' | '-'
+   rule-definition = <'Rule'> <space> rule-name <space> rule-from <space> rule-to <space> 
+                     rule-type <space> rule-in <space> rule-on <space> rule-at <space> 
+                     rule-save <space> rule-letters <space>? <newline>?
+
+
    <empty-space> = (<space> <newline> | <newline>)+
-   timezone = (rule-definition /  zone-definition / link-definition)+")
 
-(def zone-definition 
-  "# Summer Time Order 1997 (S.I. 1997/2982)
-# See EU for rules starting in 1996.
-#
-# Use Europe/London for Jersey, Guernsey, and the Isle of Man.
+   timezone = (rule-definition | zone-definition | link-definition)+")
 
-# Zone	NAME		GMTOFF	RULES	FORMAT	[UNTIL]
-Zone	Europe/London	-0:01:15 -	LMT	1847 Dec  1  0:00s
-			0:00	GB-Eire	%s	1968 Oct 27
-			1:00	-	BST	1971 Oct 31  2:00u
-			0:00	GB-Eire	%s	1996
-			0:00	EU	GMT/BST")
-
-
-(def rule-definition "Rule	GB-Eire	1916	only	-	May	21	2:00s	1:00	BST")
 (def zone-parser (insta/parser zone-grammar))
 
-(comment
-  (zone-parser rule-definition :start :rule-definition)
+(defn read-zone [zone]
   (->
-    zone-definition
+    (zone-definition zone)
     (clojure.string/replace #"#.*" "")
     clojure.string/split-lines
     (#(remove (comp empty? clojure.string/trim) %))
     (#(clojure.string/join "\n" %))
-    (zone-parser :start :zone-definition)))
-
-(defn until->date [text]
-  (let [grammar (apply str
-                       "until = year (<space> month <space> day)? (<space> time)?\n"
-                       from-to-grammar)
-        parser (insta/parser grammar)
-        result (parser text)]
-    (insta/transform
-      {:number #(Integer/parseInt %)
-       :month-name months
-       :time (fn [& args] (reduce conj {} args))
-       :until (fn [& args] (reduce conj {} args))}
-      result)))
-
-(defn parse-zone-definition [lines]
-  (let [[zd & zdl] lines
-        [_ zname gmtoff zformat zuntil] (clojure.string/split zd #"\t")
-        [gmtoff rule] (clojure.string/split gmtoff #"\s+")
-        definition {:name zname
-                    :history [{:offset gmtoff :rule rule :format zformat :until zuntil}]}]
-    (if (empty? zdl) definition
-      (reduce 
-        (fn [r [gmt rule zformat until]]
-          (update r :history conj 
-            {:offset gmt :rule rule :format zformat :until until}))
-        definition
-        (map
-          #(clojure.string/split (clojure.string/trim %) #"\t")
-          zdl)))))
-
-
-(defn parse-link-definition [line]
-  (let [[_ from to] (clojure.string/split line #"[\t\s]+")]
-    [to from]))
-
-(defn parse-rule-definition [line]
-  (let [[_ & data] (clojure.string/split line #"[\t\s]+")]
-    (zipmap 
-      [:name :from :to :type :in :on :at :save :letter]
-      data)))
-
-
+    (zone-parser :start :timezone)))
 
 (comment
-  (def ise (break-timezone-db europe))
-  (:links ise)
-  (:rules ise)
-  (def until (-> ise :zones first parse-zone-definition :history first))
-  (re-find #"(\d+)\s+(\w+)\s+(\d+)\s+(\d+):(\d+[sguz])" "1847 Dec  1  0:00s")
-  (-> ise :links first parse-link-definition)
-  (-> ise :rules first parse-rule-definition))
+  (zone-parser rule-definition :start :rule-definition)
+  (zone-parser
+    "\t\t\t 0:00\tGB-Eire\t%s\t1921 Dec  6 \n"
+    :start :zone-row)
+  (read-zone :pacificnew)
+  (zone-definition :europe)
+  (->
+    europe
+    (clojure.string/replace #"#.*" "")
+    clojure.string/split-lines
+    (#(remove (comp empty? clojure.string/trim) %))
+    (#(clojure.string/join "\n" %))
+    (zone-parser :start :timezone)))
+
+; (defn until->date [text]
+;   (let [grammar (apply str
+;                        "until = year (<space> month <space> day)? (<space> time)?\n"
+;                        from-to-grammar)
+;         parser (insta/parser grammar)
+;         result (parser text)]
+;     (insta/transform
+;       {:number #(Integer/parseInt %)
+;        :month-name months
+;        :time (fn [& args] (reduce conj {} args))
+;        :until (fn [& args] (reduce conj {} args))}
+;       result)))
+
+; (defn parse-zone-definition [lines]
+;   (let [[zd & zdl] lines
+;         [_ zname gmtoff zformat zuntil] (clojure.string/split zd #"\t")
+;         [gmtoff rule] (clojure.string/split gmtoff #"\s+")
+;         definition {:name zname
+;                     :history [{:offset gmtoff :rule rule :format zformat :until zuntil}]}]
+;     (if (empty? zdl) definition
+;       (reduce 
+;         (fn [r [gmt rule zformat until]]
+;           (update r :history conj 
+;             {:offset gmt :rule rule :format zformat :until until}))
+;         definition
+;         (map
+;           #(clojure.string/split (clojure.string/trim %) #"\t")
+;           zdl)))))
+
+
+; (defn parse-link-definition [line]
+;   (let [[_ from to] (clojure.string/split line #"[\t\s]+")]
+;     [to from]))
+
+; (defn parse-rule-definition [line]
+;   (let [[_ & data] (clojure.string/split line #"[\t\s]+")]
+;     (zipmap 
+;       [:name :from :to :type :in :on :at :save :letter]
+;       data)))
+
+
+
+; (comment
+;   (def ise (break-timezone-db europe))
+;   (:links ise)
+;   (:rules ise)
+;   (def until (-> ise :zones first parse-zone-definition :history first))
+;   (re-find #"(\d+)\s+(\w+)\s+(\d+)\s+(\d+):(\d+[sguz])" "1847 Dec  1  0:00s")
+;   (-> ise :links first parse-link-definition)
+;   (-> ise :rules first parse-rule-definition))
