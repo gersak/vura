e7c3fc8b738b73eaa00255f39d889afab611a069
Robert Geršak <robi@neyho.com>
2022-01-30 15:15:09 +0100

    Moved timezones generated to src

--- core/build.clj
+++ core/build.clj
@@ -6,16 +6,18 @@
 (def lib 'com.github.gersak/vura.core)
 (def version "0.5.7-SNAPSHOT")
 (def basis (b/create-basis {:project "deps.edn"}))
+(def target "target")
 
 (defn pom [_]
   (b/write-pom {:lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]}))
-
+                :resource-dirs ["src" "../timezones/src"]
+                :target "./"}))
 
 (defn deploy [_]
   (pom nil)
-  (b/process {:command-args ["mvn" "deploy"]}))
+  (b/process {:dir target
+              :command-args ["mvn" "deploy"]}))
 
 
